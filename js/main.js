af18940a10de6b0601f9861839eb835e25c36a07
NevenSherer <2007055@apps.nsd.org>
2024-11-13 08:56:39 -0800

    sets up stuff

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
-var guess1 = [];
-var guess2 = [];
-var guess3 = [];
-var guess4 = [];
-var guess5 = [];
-var guess6 = [];
+var correctAnswer = "hippo";
+correctAnswer = correctAnswer.toUpperCase();
+var guessCount = 1;
 
-function setLetterColors() {
-    
+function submitGuess() {
+    var inputFrame = document.getElementById("guessInput");
+    var guess = "";
+    for (var i = 0; i < inputFrame.length; i++) {
+        if (inputFrame.elements[i].length = 5){
+            guess += inputFrame.elements[i].value;
+        }
+    }
+    if (guess != "") {
+        guess = guess.toUpperCase();
+        for (var i = 0; i < guess.length; i++) {
+            if (guess.charAt(i) == correctAnswer.charAt(i)) {
+                document.getElementById(guessCount + "-" + i).classList.add("correct");
+                document.getElementById(guessCount + "-" + i).classList.remove("default");
+            }
+            else {
+                for (var j = 0; j < guess.length; j++) {
+                    if (guess.charAt(i) == correctAnswer.charAt(j)) {
+                        document.getElementById(guessCount + "-" + i).classList.add("somewhereElse");
+                        document.getElementById(guessCount + "-" + i).classList.remove("default");
+                    }
+                }
+            }
+        }
+    }
+    guessCount++;
+    console.log(guess);
+    console.log(correctAnswer);
 }
